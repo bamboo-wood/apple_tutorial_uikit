79648ae9f1de289812a098bc890f254d2d1e6846
bamboo-wood <cantate.1009.0417@gmail.com>
2023-06-05 14:00:44 +0900

    コンテンツビューを作成

---
+++ AppleTutorialUIKit/ContentViews/DatePickerContentView.swift
@@ -0,0 +1,48 @@
+//
+//  DatePickerContentView.swift
+//  AppleTutorialUIKit
+//
+//  Created by 佐々木一樹 on 2023/06/05.
+//
+
+import UIKit
+
+class DatePickerContentView: UIView, UIContentView {
+    struct Configuration: UIContentConfiguration {
+        var date = Date.now
+        
+        func makeContentView() -> UIView & UIContentView {
+            return DatePickerContentView(self)
+        }
+    }
+    
+    let datePicker = UIDatePicker()
+    
+    var configuration: UIContentConfiguration {
+        didSet {
+            configure(configuration: configuration)
+        }
+    }
+    
+    init(_ configuration: UIContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .zero)
+        addPinnedSubView(datePicker)
+        datePicker.preferredDatePickerStyle = .inline
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(configuration: UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        datePicker.date = configuration.date
+    }
+}
+
+extension UICollectionViewListCell {
+    func datePickerConfiguration() -> DatePickerContentView.Configuration {
+        DatePickerContentView.Configuration()
+    }
+}
---
+++ AppleTutorialUIKit/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  AppleTutorialUIKit
+//
+//  Created by 佐々木一樹 on 2023/06/05.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
